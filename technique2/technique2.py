a214c897803305777a201e4f2102deb54c9ba5d6
Ashved Andhale <ashvedandhale07@gmail.com>
2022-12-06 11:33:07 +0530

    Added Comments

--- technique2/technique2.py
+++ technique2/technique2.py
@@ -1,13 +1,12 @@
 # Deadcode Elimination
-import re
-inputfile = open("technique2/technique2_input.txt", "r")
-outputfile = open("technique2/technique2_output.txt", "w")
-variables_table = {}
+import re       #importing regular expressions library
+inputfile = open("technique2/technique2_input.txt", "r")    #read input file using read mode
+outputfile = open("technique2/technique2_output.txt", "w")  #create a file to store output
+variables_table = {}    #it is a dictionary to store the operands in it[Operand: used or not(true/false)]
 for line in inputfile:
     eq_separated_token = [token.strip() for token in line.strip().split('=')]
     if(len(eq_separated_token) > 1):
-        if(variables_table.get(eq_separated_token[0]) == None):
-            variables_table[eq_separated_token[0]] = False
+        variables_table[eq_separated_token[0]] = False
         operator_separated_token = [token.strip() for token in re.split(r"/\s*|\+|-|\*|\%|\/", eq_separated_token[1]) if token.strip().isalpha()]
         for var in operator_separated_token:
             variables_table[var] = True
