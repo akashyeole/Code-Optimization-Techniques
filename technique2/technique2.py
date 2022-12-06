5e0a05e96a028b01ac9a5492d48e2979718ec014
Akash <akashyeole02@gmail.com>
2022-12-06 14:03:32 +0530

    Added Comments

--- technique2/technique2.py
+++ technique2/technique2.py
@@ -5,8 +5,9 @@ outputfile = open("technique2/technique2_output.txt", "w")  #create a file to st
 variables_table = {}    #it is a dictionary to store the operands in it[Operand: used or not(true/false)]
 for line in inputfile:
     eq_separated_token = [token.strip() for token in line.strip().split('=')]   #split output based on '='
-    if(len(eq_separated_token) > 1):    
-        variables_table[eq_separated_token[0]] = False      #set unused operands as false
+    if(len(eq_separated_token) > 1): 
+        if(variables_table.get(eq_separated_token[0]) == None):     # if the variable is already present in the table then no need to add it as false kepp it as it is
+            variables_table[eq_separated_token[0]] = False      #set unused operands as false
         operator_separated_token = [token.strip() for token in re.split(r"/\s*|\+|-|\*|\%|\/", eq_separated_token[1]) if token.strip().isalpha()]       #split expression based on the operator and store the operands in the eq_separated_tokens folder
         for var in operator_separated_token:
             variables_table[var] = True     #set operands which are used/assigned as True
--- technique3/technique3.py
+++ technique3/technique3.py
@@ -2,17 +2,17 @@
 import re
 inputfile = open("technique3/technique3_input.txt", "r")
 outputfile = open("technique3/technique3_output.txt", "w")
-variables_table = {}
+variables_table = {} #For Storing Variable values
 for line in inputfile:
-    eq_separated_token = [token.strip() for token in line.strip().split('=')]
+    eq_separated_token = [token.strip() for token in line.strip().split('=')] # Split into 2 sections 
     if(len(eq_separated_token) > 1):
-        operator_separated_token = [token.strip() for token in re.split(r"/\s*|\+|-|\*|\%|\/", eq_separated_token[1]) if token.strip().isalpha()]
-        temp = eq_separated_token[1]
+        operator_separated_token = [token.strip() for token in re.split(r"/\s*|\+|-|\*|\%|\/", eq_separated_token[1]) if token.strip().isalpha()] # Seperationg the operand 
+        temp = eq_separated_token[1] # Storing all RHS Side variables
         for var in operator_separated_token:
             if(variables_table.get(var) != None):
-                temp = temp.replace(var, variables_table[var])
+                temp = temp.replace(var, variables_table[var]) # If value of var in present in variable table then replace 
         variables_table[eq_separated_token[0]] = temp
-        outputfile.write(eq_separated_token[0] + " = " + temp + "\n")
+        outputfile.write(eq_separated_token[0] + " = " + temp + "\n") # If any value is replaced then store that in temp 
     else:
         outputfile.write(line + "\n")
 inputfile.close()
