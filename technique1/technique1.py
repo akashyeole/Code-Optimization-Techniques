5ba5fc0f7deff87d421c0ce90815a9fa1f295618
Akash <akashyeole02@gmail.com>
2022-12-06 12:18:19 +0530

    Added Comments

--- technique1/technique1.py
+++ technique1/technique1.py
@@ -1,21 +1,37 @@
 # Constant Folding
 import re
-inputfile = open("technique1/technique1_input.txt", "r")
-outputfile = open("technique1/technique1_output.txt", "w")
+inputfile = open("technique1_input.txt", "r")
+outputfile = open("technique1_output.txt", "w")
+# constants_table : It stores all the constant in key-value pair
 constants_table = {}
-for line in inputfile:
+for line in inputfile :
+    # this line split all the token and make  the new list of tokens
     tokens = [token.strip() for token in line.strip().split(' ')]
+    # print(tokens)
+    # In tokens it will check  for the #define.
+    # If it is present then it then it stores the constant value  in constant_tables
     if(tokens[0] == "#define"):
         constants_table[tokens[1]] = tokens[2]
+        # print(constants_table)
     else:
+        # eq_separated : it stores the expression which is split by = and stores into the list
         eq_separated_data = [token.strip() for token in line.strip().split('=')]
+        # print(eq_separated_data)
+        #it stores the tokens separated by operator
         operator_separated_token = [token.strip() for token in re.split(r"/\s*|\+|-|\*|\%|\/", eq_separated_data[1])]
+        # print(operator_separated_token)
+        # temp : it will stored the eq_seperated token in index 1
         temp = eq_separated_data[1]
+        # print(temp)
+
         for var in operator_separated_token:
             if(constants_table.get(var) != None):
+                # it replace the var with the value in constant table
                 temp = temp.replace(var, constants_table[var])
+                # print(temp)
             
         try:
+            # it compute the expression store in into the result
             result = eval(temp)
         except:
             result = temp
--- technique4/technique4.py
+++ technique4/technique4.py
@@ -1,27 +1,26 @@
 # Unreachable code removal
-import re
+
+# Opening the Input File in read mode
 inputfile = open("technique4/technique4_input.txt", "r")
+# Opening the Output File in write mode
 outputfile = open("technique4/technique4_output.txt", "w")
-variables_table = {}
-stk = []
-f = False
+# The flag that whether the upcoming lines should be included in the output program or not
+removeFiles = False
+
+# Iterating every line of thr input program
 for line in inputfile:
-    if(line.strip().endswith("}") and f):
+    if(line.strip().endswith("}") and removeFiles):
         outputfile.write(line)
-        f = False
-    elif line.strip().endswith("()"):
+        removeFiles = False
+    elif line.strip().endswith("{") or line.strip().endswith(")") or line.strip().endswith("){"): # Check if new block starts
         outputfile.write(line)
-    elif line.strip().endswith("(){"):
-        outputfile.write(line)
-        stk += ['{']
-    elif line.strip().endswith("{"):
-        outputfile.write(line)
-        stk += ['{']
-    elif line.strip() in ["break;", "continue;", "return 0;"]:
-        if(line.strip() != "continue;"):
+    elif line.strip() in ["break;", "continue;", "return 0;"]: # If any of these statements encounter then set flag
+        if(line.strip() != "continue;"): # Don't print continue if it continue
             outputfile.write(line)
-        f = True
-    elif(not f):
+        removeFiles = True
+    elif(not removeFiles): # If flag is set to false then add lines to the output files
         outputfile.write(line)
+
+# Cloding the opened files
 inputfile.close()
 outputfile.close()
